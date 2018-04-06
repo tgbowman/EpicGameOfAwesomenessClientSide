5703dd7a6f10560d921b397f573fa1f85fa9a72b
Tyler Bowman <tylerbowman@Tylers-MacBook-Pro-2.local>
2018-04-06 11:50:46 -0500

    Started working on implementing user created adventures

---
+++ epic-game-client/src/components/CreateAdventure.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link, Redirect } from "react-router-dom";
+import Logout from "../components/Logout";
+
+class CreateAdventure extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+
+            <div>
+                <h2> Create a New Adventure! </h2>
+            </div>)
+    }
+}
